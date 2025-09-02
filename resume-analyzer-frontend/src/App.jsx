38f300783658d5604a70a1ba673f1377ad51591f
chandan786k <chandan7193kumar@gmail.com>
2025-09-02 16:10:36 +0530

    added backend link

--- resume-analyzer-frontend/src/App.jsx
+++ resume-analyzer-frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [dragActive, setDragActive] = useState(false)
   const fileInputRef = useRef(null)
 
-  const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:5000'
+  const API_BASE_URL = 'https://resume-analyzer-2-z8qa.onrender.com' || 'http://127.0.0.1:5000'
 
 
   const handleFileSelect = (selectedFile) => {
